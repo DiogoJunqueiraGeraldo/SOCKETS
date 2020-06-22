127c5fce5b9340d12984e8b84045bf0d83446d57
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2020-06-22 11:14:54 +0530

    remove unused libraries

--- TCPCLIENT/tcpc.c
+++ TCPCLIENT/tcpc.c
@@ -1,17 +1,11 @@
 #include <sys/socket.h>
-#include <sys/types.h>
-#include <signal.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <stdarg.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/time.h>
-#include <sys/ioctl.h>
-#include <netdb.h>
 
 #define SERVER_PORT 80
 #define MAX_LINE 4096
@@ -38,8 +32,9 @@ int main(int argc, char** argv)
 	if (connect(socket_network, (struct sockaddr*) &servaddr, sizeof(servaddr)) < 0)
 		err_n_die("Error while trying to connect!");
 
-	char sendline[MAX_LINE];
-	sprintf(sendline, "GET / HTTP/1.1\r\n\r\n");
+	// char sendline[MAX_LINE];
+	// sprintf(sendline, "GET / HTTP/1.1\r\n\r\n");
+	char* sendline = "GET / HTTP/1.1\r\n\r\n";
 	int sendbytes = strlen(sendline);
 
 	if (write(socket_network, sendline, sendbytes) != sendbytes)
